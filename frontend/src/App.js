3ac9c98298906bf625d180586b10c6781418bc85
Shefali-Sharma <shefali92@gmail.com>
2021-03-06 14:32:03 -0800

    Add signin form from Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body class="text-center">
+      <main class="form-signin">
+        <div className="App">
+          <form>
+
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <input type="email" class="form-control" placeholder="Email" required autofocus />
+
+            <input type="password" class="form-control" placeholder="Password" required />
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+          </form>
+        </div>
+      </main>
+    </body>
   );
 }
 
