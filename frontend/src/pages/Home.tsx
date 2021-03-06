9c21409126de9fa8c310f83ecf73f03b6d31ea68
Shefali-Sharma <shefali92@gmail.com>
2021-03-06 15:46:09 -0800

    Redirect to home page after login

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        (
+            async () => {
+                try{
+                    const response = await axios.get('http://localhost:8000/api/user');
+                    const user = response.data;
+
+                    setMessage(`Hi ${user.first_name} ${user.last_name}!`)
+
+                } catch (e) {
+                    setMessage("You are not logged in!")
+                }
+                
+            }
+        )();
+    });
+
+
     return (
         <div className="container"> 
-            You are not logged in!
+            {message}
         </div>
     );
 }
